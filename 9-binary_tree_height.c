c4750e78082dfa5f7b31d89d9434f070dd5ab159
Sebastian_Orozco <omsebas@gmail.com>
2020-11-26 03:18:07 +0000

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t height_l;
 	size_t height_r;
 
-	if (tree is NULL)
+	if (tree == NULL)
 		return (0);
 
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
